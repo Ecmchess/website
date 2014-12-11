10f3f792b9177153a7464271db393e9f82064363
System User <hoquyb@K133-7.dep-info.iutmontp.univ-montp2.fr>
2014-12-11 13:59:16 +0100

    compris sonata

--- src/ECM/Bundle/ArticleBundle/Admin/ArticleAdmin.php
+++ src/ECM/Bundle/ArticleBundle/Admin/ArticleAdmin.php
@@ -20,6 +20,7 @@ use ECM\Bundle\ArticleBundle\Entity\Article;
  * @author hoquyb
  */
 class ArticleAdmin extends Admin{
+    
     protected $baseRouteName = 'sonata_article';
     protected $baseRoutePattern = 'article';
     
@@ -28,7 +29,51 @@ class ArticleAdmin extends Admin{
     {
         $formMapper
             ->add('titre')
+            ->add('corps', 'ckeditor');
+    }
+    
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('titre')
             ->add('corps');
+    }
+    
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('titre')
+            ->add('corps')
+        ;
+    }
+ 
+//    protected function configureListFields(ListMapper $listMapper)
+//    {
+//        $listMapper
+//            ->addIdentifier('company')
+//            ->add('position')
+//            ->add('location')
+//            ->add('url')
+//            ->add('is_activated')
+//            ->add('email')
+//            ->add('category')
+//            ->add('expires_at')
+//            ->add('_action', 'actions', array(
+//                'actions' => array(
+//                    'view' => array(),
+//                    'edit' => array(),
+//                    'delete' => array(),
+//                )
+//            ))
+//        ;
+//    }
+ 
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('titre')
+            ->add('corps')
+        ;
     }
     
 }
